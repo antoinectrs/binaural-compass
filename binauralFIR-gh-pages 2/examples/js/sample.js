1a9b670afdffe5b94fdfafcefbc7c92a4c31808c
Antoine <antoinecontreras98@gmail.com>
2022-04-29 17:47:50 +0200

    good function class

--- binauralFIR-gh-pages 2/examples/js/sample.js
+++ binauralFIR-gh-pages 2/examples/js/sample.js
@@ -1,12 +1,13 @@
 
 class Sample {
     constructor(path) {
-        this.AUDIO = new (AudioContext || webkitAudioContext || mozAudioContext)(),
-        this.binauralFIRNode = null,
-        this.path = path;
+        this.audio = new (AudioContext || webkitAudioContext || mozAudioContext)(),
+            this.binauralFIRNode = null,
+            this.path = path;
+        hrtfs;
         // Create an audio context
-        // let AUDIO = new (AudioContext || webkitAudioContext || mozAudioContext)();
-        // if (!AUDIO) throw 'Web Audio API not supported';
+        // let audio = new (AudioContext || webkitAudioContext || mozAudioContext)();
+        // if (!audio) throw 'Web audio API not supported';
         // Storage let for our buffer data
         // let binauralFIRNode= null;
     }
@@ -14,35 +15,43 @@ class Sample {
     // Decode the raw sample data into a AudioBuffer
     createBufferFromData(rawData) {
         console.log('Got raw sample data from XHR');
-        this.AUDIO.decodeAudioData(rawData, function (buffer) {
+        this.audio.decodeAudioData(rawData, function (buffer) {
             // console.log(`Created newAudioBuffer ${sampleBuffer}`);
             PARAMS.sampleBuffer.push(buffer);
             console.log('Ready to play');
         });
     }
     // Create a new source node and play it
-    playSample(id, e, sampleRate) {
+    // playSample(id, e, sampleRate) {
+    playSample = (id, e, sampleRate) => {
+        // this.test()
+
+
         const sampleBuffer = PARAMS.sampleBuffer[id]
         if (sampleRate === undefined) sampleRate = 1;
-        hrtf(sampleRate);
+        this.hrtf(sampleRate);
+
         this.binauralFIRNode = new BinauralFIR({
-            audioContext: this.AUDIO
+            audioContext: this.audio
         });
         //Set HRTF dataset
         this.binauralFIRNode.HRTFDataset = hrtfs;
 
-        let sourceNode = this.AUDIO.createBufferSource();
+        let sourceNode = this.audio.createBufferSource();
         sourceNode.buffer = sampleBuffer;
         sourceNode.playbackRate.value = sampleRate;
         sourceNode.connect(this.binauralFIRNode.input);
-        this.binauralFIRNode.connect(this.AUDIO.destination);
+        this.binauralFIRNode.connect(this.audio.destination);
 
         this.binauralFIRNode.setPosition(90, 10, 1);
         sourceNode.loop = true;
         sourceNode.start(0);
         console.log('Played sample via new AudioBufferSourceNode');
     }
-    requestTrack(path,that=this) {
+    test() {
+        console.log("hello");
+    };
+    requestTrack(path, that = this) {
         // load sample
         let req = new XMLHttpRequest();
         req.responseType = "arraybuffer";
@@ -54,15 +63,11 @@ class Sample {
         req.open('GET', `../examples/snd/urban/${this.path}.mp3`, true);
         req.send();
     }
- 
+
     hrtf(sampleRate) {
-
+        // this.hrtf = function (sampleRate) {
         for (var i = 0; i < hrtfs.length; i++) {
-            console.log();
-            var buffer = this.AUDIO.createBuffer(2, 512, this.AUDIO.sampleRate);
-            // buffer = audioContext.createBuffer(2, 512, 44100);
-            // }
-
+            var buffer = this.audio.createBuffer(2, 512, this.audio.sampleRate);
             var bufferChannelLeft = buffer.getChannelData(0);
             var bufferChannelRight = buffer.getChannelData(1);
             for (var e = 0; e < hrtfs[i].fir_coeffs_left.length; e++) {
